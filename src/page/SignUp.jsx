c9a3fdfbaad7e323b8840e730924b9929e2e0533
JoSungHyeon <jsh950618@gmail.com>
2024-04-22 14:13:32 +0900

    signUp commit

--- src/page/SignUp.jsx
+++ src/page/SignUp.jsx
@@ -4,6 +4,15 @@ const SignUp = () => {
     return (
         <>
             <Header />
+            <h1>회원가입</h1>
+            <h3>아이디</h3>
+            <input type="text" />
+            <h3>이메일 주소</h3>
+            <input type="text" />
+            <h3>비밀번호</h3>
+            <input type="text" />
+            <h3>비밀번호 재입력</h3>
+            <input type="text" />
         </>
     )
 };
